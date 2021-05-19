9120d46a00a654d5389e148a805d255e4ef23513
Harshitha P <73736464+Harshithap07@users.noreply.github.com>
2021-05-19 20:50:36 +0530

    Add files via upload

---
+++ ControlFlowEx1.java
@@ -0,0 +1,16 @@
+package com.nttdata.JavaBasics;
+
+public class ControlFlowEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int aNumber=3;
+		if(aNumber>=0)
+			if(aNumber==0)
+				System.out.println("first string");
+			else
+				System.out.println("Second string");
+		System.out.println("Third string");
+	}
+}
+	
